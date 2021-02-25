7e66fabd32e1d49cf661d322892a5c552e1885ca
RianIslam <rianislamdr@gmail.com>
2021-02-26 01:34:13 +0600

    useState slice products

---
+++ src/components/Header/Shop/Shop.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import fakeData from '../../../fakeData'
+import './Shop.css'
+
+const Shop = () => {
+    const first10 = fakeData.slice(0,10);
+    const [products,setProducts] = useState(first10)
+    return (
+        <div>
+            <h3>{products.length}</h3>
+        </div>
+    )
+}
+
+export default Shop
