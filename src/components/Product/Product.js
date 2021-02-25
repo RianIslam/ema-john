6f02c39ec932d23197086d2b2cf8e3b9df0bc957
RianIslam <rianislamdr@gmail.com>
2021-02-26 02:31:27 +0600

    add img name seller price stock

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import './Product.css'
 const Product = (props) => {
-    console.log(props)
+    const {img,name,seller,price,stock} = props.product;
     return (
+        <div className="product">
         <div>
-            <h4>{props.product.name}</h4>
+            <img src={img} alt=""/>
+        </div>
+        <div>
+        <h4 className="product-name">{name}</h4>
+        <br/>
+        <p><small>by:{seller}</small></p>
+        <p>${price}</p>
+        <br/>
+        <p><small>Only {stock} left in stock - Order soon</small></p>
+        </div>
+            
         </div>
     )
 }
