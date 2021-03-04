046afa8296e095aa0b16268ced1930ad4e4ef62d
RianIslam <rianislamdr@gmail.com>
2021-03-04 23:20:58 +0600

    add existing key

--- src/components/Header/Shop/Shop.js
+++ src/components/Header/Shop/Shop.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import fakeData from "../../../fakeData";
-import { addToDatabaseCart } from "../../../utilities/databaseManager";
+import { addToDatabaseCart, getDatabaseCart } from "../../../utilities/databaseManager";
 import Cart from "../../Cart/Cart";
 import Product from "../../Product/Product";
 import "./Shop.css";
@@ -8,8 +8,18 @@ import "./Shop.css";
 const Shop = () => {
   const first10 = fakeData.slice(0, 10);
   const [products, setProducts] = useState(first10);
-
   const [cart,setCart]= useState([]);
+
+  useEffect(() => {
+    const savedCart = getDatabaseCart();
+    const productKeys = object.keys(savedCart)
+    const previousCart = productKeys.map(existingKey =>{
+      const product = fakeData.find(pd => pd.key === existingKey)
+    })
+    console.log(savedCart)
+  },[])
+
+
   
     const handleAddProduct = (product) =>{
       const toBeAddedKey = product.key;
