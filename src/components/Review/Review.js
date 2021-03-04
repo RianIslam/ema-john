b66a8d3b5dd8959185f27e7f90999e1cec258b81
RianIslam <rianislamdr@gmail.com>
2021-03-04 22:49:01 +0600

    review cart removing

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,46 +1,49 @@
-import React, { useEffect, useState } from 'react'
-import fakeData from '../../fakeData';
-import { getDatabaseCart, removeFromDatabaseCart } from '../../utilities/databaseManager';
-import ReviewItem from '../ReviewItem';
+import React, { useEffect, useState } from "react";
+import fakeData from "../../fakeData";
+import {
+  getDatabaseCart,
+  removeFromDatabaseCart,
+} from "../../utilities/databaseManager";
+import Cart from "../Cart/Cart";
+import ReviewItem from "../ReviewItem";
 
 const Review = () => {
-    const [cart,setCart] = useState([]);
+  const [cart, setCart] = useState([]);
 
-    const removeProduct =(productKey) =>{
+  const removeProduct = (productKey) => {
+    console.log("removing", productKey);
 
-        console.log('removing',productKey)
+    const newCart = cart.filter((pd) => pd.key !== productKey);
+    setCart(newCart);
+    removeFromDatabaseCart(productKey);
+  };
 
-        const newCart = cart.filter(pd => pd.key !== productKey)
-        setCart(newCart);
-        removeFromDatabaseCart(productKey);
-    }
+  useEffect(() => {
+    const savedCart = getDatabaseCart();
+    const productKeys = Object.keys(savedCart);
+    const cartProducts = productKeys.map((key) => {
+      const product = fakeData.find((pd) => pd.key === key);
+      product.quantity = savedCart[key];
+      return product;
+    });
+    setCart(cartProducts);
+  }, []);
+  return (
+    <div className="shop-container">
+      <div className="product-container">
+        {cart.map((pd) => (
+          <ReviewItem
+            key={pd.key}
+            removeProduct={removeProduct}
+            product={pd}
+          ></ReviewItem>
+        ))}
+      </div>
+      <div className="cart-cont">
+          <Cart cart={cart}/>
+      </div>
+    </div>
+  );
+};
 
-    useEffect(() =>{
-        const savedCart = getDatabaseCart();
-        const productKeys = Object.keys(savedCart)
-        const cartProducts = productKeys.map(key => {
-            const product = fakeData.find(pd => pd.key ===  key)
-            product.quantity =savedCart[key]
-            return product
-        })
-        setCart(cartProducts)
-
-    })
-    return (
-        <div>
-            <h1>THis is {cart.length}</h1>
-            {
-
-                cart.map(pd => <ReviewItem
-                key={pd.key}
-                removeProduct={removeProduct}
-                product={pd}>
-                
-                 </ReviewItem>  )
-            }
-
-        </div>
-    )
-}
-
-export default Review
+export default Review;
--- src/components/ReviewItem.js
+++ src/components/ReviewItem.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 const ReviewItem = (props) => {
-    const {name,quantity,key} = props.product
+    const {name,quantity,key,price} = props.product
     return (
         <div className="review-item">
-            <p>{name}</p>
-            <p>{quantity}</p>
+            <h3 className="product-name"> {name}</h3>
+            <p>Quantity: {quantity}</p>
+            <p><small>Price: ${price}</small></p>
             <br/>
             <button 
             onClick={() => props.removeProduct(key)}
