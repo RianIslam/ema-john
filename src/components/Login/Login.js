ba09e05407dd22b83971cb882193e9a29789c350
RianIslam <rianislamdr@gmail.com>
2021-03-16 02:40:27 +0600

    add some code

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,19 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from '../../fireBaseConfig';
 firebase.initializeApp(firebaseConfig);
 
 const Login = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
+    const[user,setUser] = useState({
+        isSignedIn: false,
+        name: '',
+        email: '',
+        photo: ''
+    });
 
+
+
+
+
+    const provider = new firebase.auth.GoogleAuthProvider();
     const handleSignIn = () => {
-        
+        firebase.auth().signInWithPopup(provider)
+        .then(res =>{
+            const {displayName,photoURL,email} =res.user;
+            const signedInUser ={
+                isSignedIn: true,
+                name: displayName,
+                email: email,
+                photo: photoURL
+            }
+            setUser(signedInUser);
+            console.log(displayName,email,photoURL);
+        })
+        .catch(err =>{
+            console.log(err);
+            console.log(err.message)
+        })
     }
 
     return (
         <div>
-        <button onClick={}>sign In</button>
+        <button onClick={handleSignIn}>sign In</button>
+        {
+            user.isSignedIn && <div>
+            <p> welcome , {user.name}</p>
+            <img src={user.photo} alt=""/>
+            <p>{user.email}</p>
+        </div>
+        }
             <h2>this is login</h2>
         </div>
     )
