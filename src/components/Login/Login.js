fe0696dbace2a1fd224a20da978b2d6d7bdb8c65
RianIslam <rianislamdr@gmail.com>
2021-03-16 02:19:37 +0600

    login form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from '../../fireBaseConfig';
+firebase.initializeApp(firebaseConfig);
 
 const Login = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    const handleSignIn = () => {
+        
+    }
+
     return (
         <div>
+        <button onClick={}>sign In</button>
             <h2>this is login</h2>
         </div>
     )
