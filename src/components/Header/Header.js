cfd77516a7b88a4cb269a4848665876b9e2ac1a5
RianIslam <rianislamdr@gmail.com>
2021-03-16 21:37:22 +0600

    header button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,6 +11,7 @@ const Header = () => {
         <Link to="/shop">Shop</Link>
         <Link to="/review">Order Review</Link>
         <Link to="/inventory">Manage Inventory</Link>
+        <button>Sign Out</button>
         </nav>
         </div>
     );
