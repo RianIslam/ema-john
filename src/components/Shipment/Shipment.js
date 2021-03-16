0d580734fde3bca2119a67a350ddc053b2d3df41
RianIslam <rianislamdr@gmail.com>
2021-03-16 21:50:17 +0600

    add react hook forom

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
 
 const Shipment = () => {
-    return (
-        <div>
-            <h2>this is Shipment</h2>
-        </div>
-    )
+    const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = data => console.log(data);
+
+  console.log(watch("example")); 
+
+  return (
+   
+    <form onSubmit={handleSubmit(onSubmit)}>
+    
+      <input name="example" defaultValue="test" ref={register} />
+      
+      
+      <input name="exampleRequired" ref={register({ required: true })} />
+      
+      {errors.exampleRequired && <span>This field is required</span>}
+      
+      <input type="submit" />
+    </form>
+  );
 }
 
 export default Shipment
