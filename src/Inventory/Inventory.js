c0a399ac0c48dee71819409e3b27db5d9157e09e
RianIslam <rianislamdr@gmail.com>
2021-03-28 04:41:05 +0600

    empty string

--- src/Inventory/Inventory.js
+++ src/Inventory/Inventory.js
@@ -3,12 +3,13 @@ import fakeData from '../fakeData'
 
 const Inventory = () => {
     const handleAddProduct=() =>{
+        const product ={}
         fetch('http://localhost:5000/addProduct',{
             method: 'POST',
             headers:{
                 'Content-Type':'application/json'
             },
-            body:JSON.stringify(fakeData)
+            body:JSON.stringify(product)
         })
     }
     return (
